4ec87799fa8453d9adcd6ff5dade5890c39a77ec
danielsantiago1002 <danielsantiago1002@hotmail.com>
2025-12-04 21:09:49 -0500

    feature: adding header and main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import "./app.css"
+
 function App() {
 
   return (
     <>
+      <header className="wellbore-visualizer-header">
+          <div>
+            <h1 className="wellbore-visualizer-header-title">
+              WellBore Visualizer
+            </h1>
+          </div>
+          <div className="wellbore-visualizer-header-links">
+          <span>
+            <a href="https://www.linkedin.com/in/danielavila1002/" target="_blank" rel="noopener noreferrer">
+              <img src="/src/assets/linkedin-svgrepo-com.svg" alt="linkedin" style={{
+                display: "inline-block",
+                width: "30px"
+              }} />
+            </a>
+          </span>
+          <span>
+            <a href="https://github.com/danielsantiago1002" target="_blank" rel="noopener noreferrer">
+              <img src="/src/assets/github-svgrepo-com.svg" alt="linkedin" style={{
+                display: "inline-block",
+                width: "30px"
+              }} />
+            </a>
+          </span>
+        </div>
+      </header>
+      <main className="wellbore-visualizer-main-container">
+
+      </main>
     </>
   )
 }
